aca1b3712613d10bb6ba06d58013082e89453dcf
szopszop <szymontracz1@gmail.com>
2023-02-13 16:11:18 +0100

    first commit

---
+++ src/main/java/com/example/spring6oauth2/Spring6Oauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring6oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6Oauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6Oauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring6oauth2/Spring6Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring6oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6Oauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
